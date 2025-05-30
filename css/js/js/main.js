e58248921e855f36c2e797092075ae4423936c59
maeloisaaa <mecs3@aluno.ifnmg.edu.br>
2025-05-30 18:45:26 +0000

    Mensagem commmit

--- css/js/js/main.js
+++ css/js/js/main.js
@@ -1,3 +1,26 @@
 function criarPresente() {
-  alert("Presente criado com sucesso!");
+  const foto = document.querySelector('input[type="file"][accept="image/*"]').files[0];
+  const musica = document.querySelector('input[type="file"][accept="audio/*"]').files[0];
+  const emocao = document.querySelector('textarea').value;
+  const data = document.querySelector('input[type="date"]').value;
+
+  if (!foto || !musica || !emocao || !data) {
+    alert('Por favor, preencha todos os campos.');
+    return;
+  }
+
+  const presente = {
+    foto: URL.createObjectURL(foto),
+    musica: URL.createObjectURL(musica),
+    emocao,
+    data
+  };
+
+  const link = gerarLinkPresente(presente);
+  alert(`Presente criado com sucesso! Compartilhe o link: ${link}`);
+}
+
+function gerarLinkPresente(presente) {
+  const base64 = btoa(JSON.stringify(presente));
+  return `https://seusite.com/presente/${base64}`;
 }
